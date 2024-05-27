bd0c3091eb704b56a4c95958de57e05b57255d58
m4teo <m4teoarg@gmail.com>
2024-05-27 20:40:56 -0300

    init

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,29 +5,59 @@
 local M = {}
 
 M.ui = {
-  theme = "mateo",
+  theme = "onedark",
   -- cmp = {
   --  style = "atom",
   --},
 
-  statusline = {
-    theme = "vscode_colored",
-    separator_style = "block",
-  },
+  -- statusline = {
+  --  theme = "vscode_colored",
+  --  separator_style = "block",
+  --  order = {
+  --    "mode",
+  --    "tint",
+  --    "separator",
+  --    "file",
+  --    "%=",
+  --    "git",
+  --    "%=",
+  --    "lsp_msg",
+  --    "%=",
+  --    "diagnostics",
+  --    "lsp",
+  --    "separator",
+      -- "force",
+      -- "file",
+  --    "cursor",
+  --    "cwd",
+  --  },
+  --  modules = {
+  --    separator = function()
+  --      return " "
+  --    end,
+      -- Force grey on modules that absorb neighbour colour
+      -- (because they don't have a highlight set)
+  --    tint = function()
+  --      return "%#StText#"
+  --    end,
+  --    force = function()
+  --      return "%#@comment#%"
+  --    end,
+  --  },
+  --},
 
-   tabufline = {
-    lazyload = false,
-  },
+  -- tabufline = {
+  --  lazyload = false,
+  --},
 
   -- cheatsheet = { theme = "simple" },
-
-  M.plugins == "custom.plugins",
+  --
+  -- M.plugins == "custom.plugins",
 
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
   },
-
 }
 
 return M
